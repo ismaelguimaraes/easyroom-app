c8b6170a563daa2b05e5d92b26c6cd90fd395f05
Ismael Borba <comercial@pyxisweb.com.br>
2022-11-01 02:31:38 -0300

    feat: autenticação do app

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,9 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
 import * as AuthSession from 'expo-auth-session';
 
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
+
 interface AuthProviderProps {
     children: ReactNode;
 }
@@ -31,7 +34,6 @@ function AuthProvider({ children }: AuthProviderProps) {
 
     async function signInWithGoogle() {
         try {
-            const { CLIENT_ID, REDIRECT_URI } = process.env;
             const RESPONSE_TYPE = 'token';
             const SCOPE = encodeURI('profile email');
 
